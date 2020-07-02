e0ab3a02e81fa292d7e8ea8129d4e7fafbb257bb
alan-cxh <cxh_928765@163.com>
2020-07-02 15:03:27 +0800

    init

---
+++ src/main/java/com/alan/socketProxy/client/Socket5ProxyClientUtil.java
@@ -0,0 +1,52 @@
+package com.alan.socketProxy.client;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Socket;
+
+/**
+ * @author chenxinhui
+ * @Description: socket客户端
+ * @date 2020/7/2
+ */
+public class Socket5ProxyClientUtil {
+
+    private static String address = "127.0.0.1";
+    private static int port = 8888;
+
+    /**
+     * 单元测试
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress remoteAddress = new InetSocketAddress("192.168.60.132", 8787);
+//        InetSocketAddress remoteAddress = new InetSocketAddress("10.1.8.20", 32010);
+        Socket5ProxyClientUtil.start(remoteAddress);
+    }
+
+
+    /**
+     * 程序入口
+     *
+     * @param remoteAddress 远程访问配置
+     * @throws IOException
+     */
+    public static void start(InetSocketAddress remoteAddress) throws IOException {
+        Socket socket = null;
+        try {
+            InetSocketAddress proxyAddress = new InetSocketAddress(address, port);
+            Proxy proxy = new Proxy(Proxy.Type.SOCKS, proxyAddress);
+            socket = new Socket(proxy);
+            socket.connect(remoteAddress);
+        } catch (IOException e) {
+            e.getStackTrace();
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+
+        }
+    }
+}
---
+++ src/main/java/com/alan/socketProxy/server/SocketForwarding.java
@@ -0,0 +1,103 @@
+package com.alan.socketProxy.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author chenxinhui
+ * @Description: 用来连接客户端和目标服务器转发流量
+ * @date 2020/7/1
+ */
+public class SocketForwarding {
+    /**
+     * 客户端socket
+     */
+    private Socket clientSocket;
+    private String clientIp;
+    /**
+     * 目标地址socket
+     */
+    private Socket targetSocket;
+    private String targetAddress;
+    private int targetPort;
+
+    public SocketForwarding(Socket clientSocket, Socket targetSocket) {
+        this.clientSocket = clientSocket;
+        this.clientIp = clientSocket.getInetAddress().getHostAddress();
+        this.targetSocket = targetSocket;
+        this.targetAddress = targetSocket.getInetAddress().getHostAddress();
+        this.targetPort = targetSocket.getPort();
+    }
+
+    public void start() {
+        OutputStream clientOs = null;
+        InputStream clientIs = null;
+        InputStream targetIs = null;
+        OutputStream targetOs = null;
+        long start = System.currentTimeMillis();
+        try {
+
+            clientOs = clientSocket.getOutputStream();
+            clientIs = clientSocket.getInputStream();
+            targetOs = targetSocket.getOutputStream();
+            targetIs = targetSocket.getInputStream();
+
+            // 512K，因为会有很多个线程同时申请buff空间，所以不要太大以以防OOM
+            byte[] buff = new byte[1024 * 512];
+            while (true) {
+
+                boolean needSleep = true;
+                while (clientIs.available() != 0) {
+                    int n = clientIs.read(buff);
+                    targetOs.write(buff, 0, n);
+                    transientLog("client to remote, bytes=%d", n);
+                    needSleep = false;
+                }
+
+                while (targetIs.available() != 0) {
+                    int n = targetIs.read(buff);
+                    clientOs.write(buff, 0, n);
+                    transientLog("remote to client, bytes=%d", n);
+                    needSleep = false;
+                }
+
+                if (clientSocket.isClosed()) {
+                    transientLog("client closed");
+                    break;
+                }
+
+                // 会话最多30秒超时，防止有人占着线程老不释放
+                if (System.currentTimeMillis() - start > 30 * 1000) {
+                    transientLog("time out");
+                    break;
+                }
+
+                // 如果本次循环没有数据传输，说明管道现在不繁忙，应该休息一下把资源让给别的线程
+                if (needSleep) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            transientLog("conn exception" + e.getMessage());
+        } finally {
+            Socks5ProxyServer.close(clientIs);
+            Socks5ProxyServer.close(clientOs);
+            Socks5ProxyServer.close(targetIs);
+            Socks5ProxyServer.close(targetOs);
+            Socks5ProxyServer.close(clientSocket);
+            Socks5ProxyServer.close(targetSocket);
+        }
+        transientLog("done.");
+    }
+
+    private void transientLog(String format, Object... args) {
+        Socks5ProxyServer.log("forwarding, clientIp=" + clientIp + ", targetAddress=" + targetAddress + ", port=" + targetPort + ", " + format, args);
+    }
+}
